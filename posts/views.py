b46dc47b0f8964b83c66f3d3565f235f6101f040
lrpohlmann <leo.pohlmann@hotmail.com>
2020-12-25 12:40:31 -0300

    Commit inicial

---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def getPaginaIndex(request):
+    pass
+
+def getPaginaDePostEspecifico(request, post_id):
+    pass
+
+def getPaginaDePostsFiltradosPorCriterio(request):
+    pass
